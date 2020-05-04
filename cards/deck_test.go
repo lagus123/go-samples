f3ed2253605f4295da30bfab7a85d5caec117c91
Jhon Laguna <jhonlag@gmail.com>
2020-05-04 17:07:09 -0500

    Added unit tests

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	deck := newDeck()
+
+	if len(deck) != 52 {
+		t.Errorf("Expected deck length of 52, but got %d", len(deck))
+	}
+
+	if deck[0] != "Ace of Spades" {
+		t.Errorf("Expected first element card of Ace of Spades, but got %s", deck[0])
+	}
+
+	if deck[len(deck)-1] != "King of Clubs" {
+		t.Errorf("Expected first element card of King of Clubes, but got %s", deck[0])
+	}
+
+}
+
+func TestDeckFileMethods(t *testing.T) {
+	os.Remove("__decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("__decktesting")
+
+	loadedDeck := newDeckFromFile("__decktesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected 52 cards on deck")
+	}
+
+	os.Remove("__decktesting")
+}
