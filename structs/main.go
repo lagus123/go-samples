a77853e011c85213aa17ffc3dd0adc79db31a18a
Jhon Laguna <jhonlag@gmail.com>
2020-05-05 13:25:58 -0500

    Added method by reference

--- structs/main.go
+++ structs/main.go
@@ -1,17 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type person struct {
 	firstName string
 	lastName  string
+	contact
+}
+
+type contact struct {
+	email string
+	zip   int
 }
 
 func main() {
 	// Basic sintax to create structs
-	jhon := person{"Jhon", "Laguna"}
+	jhon := person{"Jhon", "Laguna", contact{"jhonlag@gmail.com", 11001}}
 	// With Properties names
-	diana := person{firstName: "Diana", lastName: "Martinez"}
+	diana := person{
+		firstName: "Diana",
+		lastName:  "Martinez",
+		contact: contact{
+			email: "diana@gmail.com",
+			zip:   11001,
+		},
+	}
 
-	fmt.Println(jhon.firstName, diana.firstName)
+	jhon.print()
+	diana.print()
+
+	jhon.updateName("Edwar")
+	jhon.print()
+}
+
+func (p *person) updateName(name string) {
+	p.firstName = name
+}
+
+func (p person) print() {
+	fmt.Printf("%+v\n", p)
 }
