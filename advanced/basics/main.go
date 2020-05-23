3f6e36d69e1d0650279c96bf76e738d40eac655c
Jhon Laguna <jhonlag@gmail.com>
2020-05-23 16:32:17 -0500

    Fmt package

--- advanced/basics/main.go
+++ advanced/basics/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+var y = 30
 
 func main() {
-	fmt.Println("Hello From Go")
+	// Println returns number of bytes written and error (which is ignored)
+	n, _ := fmt.Println("Hello, playground", 42, true)
+	// Prints number of bytes written
+	fmt.Println(n)
+
+	// Print with formatting
+	fmt.Printf("%#x\n%b\n%+v \n", y, y, y)
+
+	// Prints to string with formatting
+	s := fmt.Sprintf("%#x\n%b\n%+v", y, y, y)
+
+	fmt.Println(s)
 }
