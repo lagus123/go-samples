9eef57de1c6235b6a1fc79d0ca0cea873a3f952e
Jhon Laguna <jhonlag@gmail.com>
2020-06-12 15:46:38 -0500

    Added maps exercise

--- advanced/exercises/06/main.go
+++ advanced/exercises/06/main.go
@@ -1,5 +1,24 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	m := map[string][]string{
+		"Jhon":  []string{"Movie", "Culture"},
+		"Diana": []string{"Dance", "Culture"},
+	}
+
+	m["Jess"] = []string{"Movies", "Medicine"}
+
+	delete(m, "Diana")
+
+	for k, v := range m {
+		fmt.Println(k)
+		for _, v2 := range v {
+			fmt.Print(v2 + "\t")
+		}
+		fmt.Print("\n \n")
+	}
 
 }
