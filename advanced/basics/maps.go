c0bec31e9583d4543abd83910c06ec438956cbbb
Jhon Laguna <jhonlag@gmail.com>
2020-06-12 14:22:07 -0500

    Added maps example

--- advanced/basics/maps.go
+++ advanced/basics/maps.go
@@ -1,1 +1,27 @@
+package main
 
+import "fmt"
+
+func main() {
+
+	m := map[string]int{
+		"John":  30,
+		"Diana": 27,
+	}
+
+	fmt.Println(m)
+	fmt.Println(m["John"])
+	// If the values is not present a map return Zero Value
+	fmt.Println(m["James"])
+
+	// For that we need use a trick call "comma ok" idiom
+	if _, ok := m["John"]; ok {
+		fmt.Println(m["John"])
+	}
+
+	// Print key - values
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+
+}
--- advanced/basics/slices.go
+++ advanced/basics/slices.go
@@ -6,5 +6,15 @@ func main() {
 	// Values from same types
 	x := []int{1, 3, 5, 9}
 
+	for _, v := range x {
+		fmt.Println(v)
+	}
+
+	// Slice values from slice
+	// [pos1: pos2] pos1 start position pos2 final position, upto but not includind
+	fmt.Println(x[0:3])
+
+	// Append values to slice
+	x = append(x, 4, 7)
 	fmt.Println(x)
 }
